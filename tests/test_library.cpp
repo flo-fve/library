c7b6ae4975d9d1766c62bee52952c7776d783780
Florian Favre <florian.nmf@gmail.com>
2026-07-03 12:34:09 +0200

    Update tests for library search

--- tests/test_library.cpp
+++ tests/test_library.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include <format>
+
 #include "Book.hpp"
 #include "Library.hpp"
 
@@ -83,4 +85,89 @@ TEST(LibraryTest, ReturnBookTest) {
 
     // Return available book
     EXPECT_FALSE(library.returnBook("Author 2", "Another Title 2"));
+}
+
+TEST(LibraryTest, SearchByAuthorTest) {
+    Library library;
+
+    // Add books to the library
+    Book book1("Title 1", "Author 1", 2026, true);
+    Book book2("Title 1", "Author 2", 2022, true);
+    Book book3("Title 2", "Author 2", 2023, true);
+    Book book4("Title 3", "Author 2", 2024, true);
+    library.addBook(book1);
+    library.addBook(book2);
+    library.addBook(book3);
+    library.addBook(book4);
+
+    // Search books from Author 1
+    std::vector<Book> results_1 = library.searchByAuthor("Author 1");
+
+    ASSERT_EQ(results_1.size(), 1) << "Author 1 should have 1 book";
+
+    EXPECT_EQ(results_1[0].getAuthor(), "Author 1") << "The author should be `Author 1`";
+    EXPECT_EQ(results_1[0].getTitle(), "Title 1") << "The title should be `Title 1`";
+    EXPECT_EQ(results_1[0].getYear(), 2026) << "The year should be `2026`";
+
+    // Search books from Author 2
+    std::vector<Book> results_2 = library.searchByAuthor("Author 2");
+
+    ASSERT_EQ(results_2.size(), 3) << "Author 2 should have 3 books";
+
+    EXPECT_EQ(results_2[0].getAuthor(), "Author 2") << "The author should be `Author 2`";
+    for (int i = 0; i < 3; i++) {
+        EXPECT_EQ(results_2[i].getTitle(), std::format("Title {}", i + 1))
+            << std::format("The title should be `Title {}`", i + 1);
+        EXPECT_EQ(results_2[i].getYear(), 2022 + i)
+            << std::format("The year should be `{}`", 2022 + i);
+    }
+
+    // Search books from Author 3
+    std::vector<Book> results_3 = library.searchByAuthor("Author 3");
+
+    ASSERT_EQ(results_3.size(), 0) << "Author 3 should have 0 book";
+}
+
+TEST(LibraryTest, SearchByTitleTest) {
+    Library library;
+
+    // Add books to the library
+    Book book1("Title 1", "Author 1", 2026, true);
+    Book book2("Title 1", "Author 2", 2022, true);
+    Book book3("Title 2", "Author 2", 2023, true);
+    Book book4("Title 3", "Author 2", 2024, true);
+    library.addBook(book1);
+    library.addBook(book2);
+    library.addBook(book3);
+    library.addBook(book4);
+
+    // Search books named Title 1
+    std::vector<Book> results_1 = library.searchByTitle("Title 1");
+
+    ASSERT_EQ(results_1.size(), 2) << "There should be 2 books named `Title 1`";
+
+    EXPECT_EQ(results_1[0].getTitle(), "Title 1") << "The title should be `Title 1`";
+    EXPECT_EQ(results_1[1].getTitle(), "Title 1") << "The title should be `Title 1`";
+
+    EXPECT_EQ(results_1[0].getAuthor(), "Author 1") << "The author should be `Author 1`";
+    EXPECT_EQ(results_1[1].getAuthor(), "Author 2") << "The author should be `Author 2`";
+
+    EXPECT_EQ(results_1[0].getYear(), 2026) << "The year should be `2026`";
+    EXPECT_EQ(results_1[1].getYear(), 2022) << "The year should be `2022`";
+
+    // Search books named Title 2
+    std::vector<Book> results_2 = library.searchByTitle("Title 2");
+
+    ASSERT_EQ(results_2.size(), 1) << "There should be 1 book named `Title 2`";
+
+    EXPECT_EQ(results_2[0].getTitle(), "Title 2") << "The title should be `Title 2`";
+
+    EXPECT_EQ(results_2[0].getAuthor(), "Author 2") << "The author should be `Author 2`";
+
+    EXPECT_EQ(results_2[0].getYear(), 2023) << "The year should be `2023`";
+
+    // Search books named Title X
+    std::vector<Book> results_3 = library.searchByTitle("Title X");
+
+    ASSERT_EQ(results_3.size(), 0) << "There should be no book named `Title X`";
 }
